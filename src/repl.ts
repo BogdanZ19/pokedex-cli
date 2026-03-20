42ad8df978636558aecbb59f2fe92df9b7e29782
BogdanZ19 <bogdanzainia1@gmail.com>
2026-03-20 20:24:49 +0200

    create basic repl functionality

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,28 @@
+import { createInterface } from "node:readline";
+
 export function cleanInput(input: string): string[] {
-    return input.trim().split(" ");
+    return input.trim().toLowerCase().split(" ");
+}
+
+export function startREPL() {
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: "Pokedex > "
+    });
+
+
+    rl.prompt();
+    rl.on('line', (input) => {
+        if (input === "") {
+            rl.prompt();
+            return;
+        }
+
+        const words = cleanInput(input)
+        
+        console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    });
+
 }
