d7fb8cf0cf8736499eebad794c23cb2792a03af5
BogdanZ19 <bogdanzainia1@gmail.com>
2026-03-21 17:29:08 +0200

    implement command logic (and help + exit commands)

--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,6 @@
 import { createInterface } from "node:readline";
+import { commandExit, commandHelp, getCommands } from "./commands.js";
+
 
 export function cleanInput(input: string): string[] {
     return input.trim().toLowerCase().split(" ");
@@ -19,9 +21,20 @@ export function startREPL() {
             return;
         }
 
-        const words = cleanInput(input)
+        const commands = getCommands();
+        const words = cleanInput(input);
+
+        if (commands[words[0]]) {
+            try {
+                commands[words[0]].callback(commands);
+            } catch (error) {
+                console.log(error);
+            }
+        } else {
+            console.log("Unknown command")
+        }
         
-        console.log(`Your command was: ${words[0]}`);
+        
         rl.prompt();
     });
 
